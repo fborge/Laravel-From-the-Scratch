5f288b7f43a31d26b5a982c391eeee31dd1b534e
fborges.v6 <fborges.v6@gmail.com>
2022-10-11 15:21:00 -0600

    Changing routes for porject

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,18 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('posts');
+ 
+    return view('posts', [
+        'posts' => Post::all()
+    ]);
 });
 
 Route::get('posts/{post}', function ($slug) {
+       return view('post', [
+        'post'=> Post::find($slug)
+    ]); 
     
-    if (! file_exists($path = __DIR__ . "/../resources/posts/{$slug}.html")) {
-        return redirect('/');
-    }
-    $post = cache()->remember("posts.{$slug}", 1200, fn() => file_get_contents($path));
-
-    
-    return view('post', ['post'=> $post]);
 })->where('post', '[A-z_\-]+');
 
 

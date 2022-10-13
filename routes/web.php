dbef4fef64a58ad8581f24b9481683907b466f3c
fborges.v6 <fborges.v6@gmail.com>
2022-10-13 00:19:24 -0600

    Update file

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 use App\Models\Post;
+use Spatie\YamlFrontMatter\YamlFrontMatter;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,8 +15,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
- 
-    return view('posts', [
+   
+   return view('posts', [
         'posts' => Post::all()
     ]);
 });
